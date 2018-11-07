00b46d01d088096cb1a1328d40bd41c2ecd70af9
Cheng Li <cheng.li@chenglabs.net>
2018-11-07 15:24:46 +0100

    Checked linting options

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 const port = 4000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (_req, res) => res.send('Hello World!'));
 
 app.listen({ port: 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:${port}`)
