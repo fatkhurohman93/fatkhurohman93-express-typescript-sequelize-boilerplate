1aa406aa3716ffde2ea6189d6adceaa0978772d7
Fatkhurohman Akbar <73887481+fatkhurohman93@users.noreply.github.com>
2021-12-30 09:08:13 +0700

    Add files via upload

---
+++ src/utils/base64ToImage.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import logger from '@loaders/logger';
+import { ServerError } from '@utils//appError';
+
+export default (data: string, name: string) => {
+  try {
+    logger.info('Saving image...');
+    const time = new Date().getTime();
+    const fileName = `${name}-${time}.png`;
+    const processData = data.split(',')[1];
+
+    fs.writeFileSync(`public/images/${fileName}`, processData, 'base64');
+
+    logger.info('Image saved successfully');
+
+    return fileName;
+  } catch (err) {
+    logger.error(`Failed to save image. ${err.name}: ${err.message}`);
+    throw new ServerError('Failed to save image.');
+  }
+};
---
+++ src/utils/getLocalIP.ts
@@ -0,0 +1,16 @@
+import { networkInterfaces } from 'os';
+
+export default () => {
+  const nets = networkInterfaces();
+
+  const results: string[] = []; 
+  
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      if (net.family === 'IPv4' && !net.internal) {
+        results.push(net.address);
+      }
+    }
+  }
+  return results[0];
+};
