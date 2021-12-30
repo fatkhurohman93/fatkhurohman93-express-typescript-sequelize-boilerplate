ba4b8c8d7c32e64d9de7d6549b1f19478f1fbaa6
Fatkhurohman Akbar <73887481+fatkhurohman93@users.noreply.github.com>
2021-12-30 09:18:13 +0700

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import Auth from './routers/auth.router';
+import Auth from '@routers/auth.router';
 
 export default () => {
   const app = Router();
 
   Auth(app);
+ 
   return app;
 };
